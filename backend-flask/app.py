fad4ef9ff979682c90449fd7a2b973b4c5b7b9f0
Christopher Ruediger <christopher.b.ruediger@gmail.com>
2023-03-25 19:09:58 +0000

    Added instrumentation for rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -33,6 +33,12 @@ from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProces
 #from aws_xray_sdk.core import xray_recorder
 #from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
+# import for rollbar intrumentation
+import os
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 
 # Honeycomb -----------------
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -50,6 +56,25 @@ tracer = trace.get_tracer(__name__)
 
 # Regular flask/not honeycomb
 app = Flask(__name__)
+
+
+# Rollbar ------
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 # Honeycomb -----------------
 # Initialize automatic instrumentation with Flask
@@ -81,6 +106,12 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
