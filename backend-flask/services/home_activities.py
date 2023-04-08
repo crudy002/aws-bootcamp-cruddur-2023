c30c87934aa59ff5e948418d4cc44b6d8c3e7c43
Christopher Ruediger <christopher.b.ruediger@gmail.com>
2023-04-08 22:55:18 +0000

    Got craet activity via crud post working, had to add de-hard code andrewbrown user handle

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,38 +1,12 @@
 from datetime import datetime, timedelta, timezone
 import logging
 
-from lib.db import pool, query_wrap_array
+from lib.db import db
 
 class HomeActivities:
   #def run():
   #def run(logger):
   def run(cognito_user_id=None):
-    sql = query_wrap_array("""
-      SELECT
-        activities.uuid,
-        users.display_name,
-        users.handle,
-        activities.message,
-        activities.replies_count,
-        activities.reposts_count,
-        activities.likes_count,
-        activities.reply_to_activity_uuid,
-        activities.expires_at,
-        activities.created_at
-      FROM public.activities
-      LEFT JOIN public.users ON users.uuid = activities.user_uuid
-      ORDER BY activities.created_at DESC
-    """)
-    print("SQL--------------")
-    print(sql)
-    print("SQL--------------")
-    with pool.connection() as conn:
-      with conn.cursor() as cur:
-        cur.execute(sql)
-        # this will return a tuple
-        # the first field being the data
-        json = cur.fetchone()
-    print("-1----")
-    print(json[0])
-    return json[0]
-    return resultS
+    sql = db.template('activities','home')
+    results = db.query_array_json(sql)
+    return results
