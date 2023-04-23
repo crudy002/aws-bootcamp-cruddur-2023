d0bbf0c43c88a6bb2bad9e04684a431aeba540cc
Christopher Ruediger <christopher.b.ruediger@gmail.com>
2023-04-23 18:39:06 +0000

    Reorganized scripts in bin and added health check and postgres test scripts

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -69,23 +69,27 @@ cognito_jwt_token = CognitoJwtToken(
   region=os.getenv("AWS_DEFAULT_REGION")
 )
 
-# Rollbar ------
-rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
-@app.before_first_request
-def init_rollbar():
-    """init rollbar module"""
-    rollbar.init(
-        # access token
-        rollbar_access_token,
-        # environment name
-        'production',
-        # server root directory, makes tracebacks prettier
-        root=os.path.dirname(os.path.realpath(__file__)),
-        # flask already sets up logging
-        allow_logging_basic_config=False)
 
-    # send exceptions from `app` to rollbar, using flask's signal system.
-    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+
+# # Rollbar ------
+
+# rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+# @app.before_first_request
+# def init_rollbar():
+#     """init rollbar module"""
+#     rollbar.init(
+#         # access token
+#         rollbar_access_token,
+#         # environment name
+#         'production',
+#         # server root directory, makes tracebacks prettier
+#         root=os.path.dirname(os.path.realpath(__file__)),
+#         # flask already sets up logging
+#         allow_logging_basic_config=False)
+
+#     # send exceptions from `app` to rollbar, using flask's signal system.
+#     got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 # Honeycomb -----------------
 # Initialize automatic instrumentation with Flask
@@ -120,10 +124,14 @@ cors = CORS(
 )
 
 
-@app.route('/rollbar/test')
-def rollbar_test():
-    rollbar.report_message('Hello World!', 'warning')
-    return "Hello World!"
+@app.route('/api/health-check')
+def health_check():
+  return {'success': True}, 200
+
+# @app.route('/rollbar/test')
+# def rollbar_test():
+#     rollbar.report_message('Hello World!', 'warning')
+#     return "Hello World!"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
--- journal/week6.md
+++ journal/week6.md
@@ -1,1 +1,6 @@
 # Week 6 — Deploying Containers
+
+- Set up srcipt to test connection to RDS
+    - Make sure to start DB
+- Set up script to test api endpoints (health-check)
+    - Make sur the container is running
